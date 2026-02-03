1e91b11967c130ddaa937d3d6e84eabb38acc687
horang52 <167264939+horang0210@users.noreply.github.com>
2026-02-03 21:00:35 +0900

    Add wedding photo and update GallerySection

--- src/sections/GallerySection.tsx
+++ src/sections/GallerySection.tsx
@@ -2,6 +2,11 @@ const images = [
     "/images/1.jpg",
     "/images/2.jpg",
     "/images/3.jpg",
+    "/images/4.jpg",
+    "/images/5.jpg",
+    "/images/6.jpg",
+    "/images/7.jpg",
+    "/images/8.jpg",
   ];
   
   export default function GallerySection() {
